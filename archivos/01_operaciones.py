837f37d7d9f00ed127e87e1205d5e54f3e4d30ae
alexscamg <alejandrosanchezsk8@gmail.com>
2022-11-21 18:45:28 +0100

    Commit!

--- archivos/01_operaciones.py
+++ archivos/01_operaciones.py
@@ -1,7 +1,11 @@
 """
 En python para abrir archivos con poner open ya nos abre el archivo
 """
-# r, w, a, w+, r+, a+, b
+# r, w, a, 
+# w+(si existe se borra, si no se crea)
+# r+ (si no existe da error)
+# a+(si existe se añade por el final)
+# b
 archivo = open('/Users/alejandrosanchezcaballero/Desktop/DAM/Programacion/Python_01/archivos/pruebas.txt', 'r')
 numeros = []
 
