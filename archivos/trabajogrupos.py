580da60350b213a55a120ebae7aeb74558340f1d
alexscamg <alejandrosanchezsk8@gmail.com>
2022-11-23 19:37:52 +0100

    Commit!

--- archivos/trabajogrupos.py
+++ archivos/trabajogrupos.py
@@ -8,18 +8,45 @@
 
 tareas_pendientes = []
 
-
-
-
 '''
-tareas_hechas = []
-tareas_pendientes = []
-tpd = {}
-thd = {}
+
+
+
 ruta_base= '/home/alejandro/Proyectos/Python_01/archivos/'
-pendientes = ruta_base + 'pendientes.txt'
-hechos = ruta_base + 'hechos.txt'
-def dime_tarea (): 
+pendientes = ruta_base + 'tareaspendientes.txt'
+hechos = ruta_base + 'tareasterminadas.txt'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" def t_pendientes (): 
+  tareas_pendientes = []
   while True:
 
     tarea = input('introduce la tarea a realizar: ')
@@ -27,42 +54,27 @@ def dime_tarea ():
   
     if tarea == '':
       break
+  tareas_pendientes.pop()
+  return tareas_pendientes
 
 
 
-def id_pen(*tareas_pendientes):
-  
-  for t in range(len(tareas_pendientes)):
-    tpd[t] = tareas_pendientes[t]
-  
-  return tpd
-
-
-
-def lista_h():
+def t_terminadas ():
+  tareas_hechas = []
   while True:
     tarea_hecha = input('Dime qué tareas has terminado: ')
-    if tarea_hecha in tareas_pendientes:
+    if tarea_hecha in t_pendientes:
       tareas_hechas.append(tarea_hecha) 
-      tareas_pendientes.remove(tarea_hecha)
+      t_pendientes.remove(tarea_hecha)
     else:
       break
+  return tareas_hechas
+
+
+t_pendientes()
+
+t_terminadas()
 
 
 
-
-
-
-
-dime_tarea()
-tareas_pendientes.pop()
-id_pen()
-lista_h()
-
-print(tareas_pendientes)
-print(id_pen(*tareas_pendientes))
-print(tareas_hechas)
-
-'''  
-  tareas = input('Dime qué tareas tienes: ')
-'''
+ """
