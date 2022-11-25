3c1f24f6415cbfeef219c64e2a90b3a9b5073c6d
alexscamg <alejandrosanchezsk8@gmail.com>
2022-11-25 20:01:57 +0100

    Commit!

--- Ejercicios Clase/04_cuenta_letras.py
+++ Ejercicios Clase/04_cuenta_letras.py
@@ -6,18 +6,20 @@ Los caracteres que no son letras se descartan.
 """
 
 def contador():
-  entrada = input('Introduce una letra: ')
   vocales = 'aeiou'
   consonantes = 'BCDFGHJKLMNÑPQRSTVXZWY'
   lista_vocales = []
   lista_consonantes = []
-  while entrada != '':
+  while True:
+    entrada = input('Introduce una letra: ')
+    if entrada == '':
+      break
     if entrada.lower() in vocales:
       lista_vocales.append(entrada)
 
     elif entrada.upper() in consonantes:
       lista_consonantes.append(entrada)
-    entrada = input('Introduce una letra: ')
+    
   
   return lista_vocales , lista_consonantes
 
