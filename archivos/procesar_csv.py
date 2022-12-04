8c68215816a62c96fd671334e6f434a1e25d1186
alexscamg <alejandrosanchezsk8@gmail.com>
2022-12-04 18:29:32 +0100

    Commit!

--- archivos/procesar_csv.py
+++ archivos/procesar_csv.py
@@ -1,5 +1,7 @@
 import csv
-
+import pprint
+import os
+from heapq import nlargest
 
 """
 # 1. % de ventas de cada vendedor
@@ -12,7 +14,7 @@ RUTA_BASE = '/Users/alejandrosanchezcaballero/Desktop/DAM/Programacion/Python_01
 archivo = RUTA_BASE + 'cust_orders_prods-cust_orders_prods.csv'
 
 
-def leer_archivo(archivo):
+def leer_archivo(archivo):              # Leer el archivo para procesar los datos
     resultado = {}
     contador = 0
     with open(archivo, 'r') as csv_file:
@@ -23,37 +25,33 @@ def leer_archivo(archivo):
     return resultado
 
 
-def total(datos):
+def total(datos):                     # Calcula el total de las cantidades
     total = 0
     for elem in datos:
         cantidad = datos[elem]["quantity"].replace('.', '')
         total += int(cantidad)
-    print(total)
     return total
 
 
-def nombre_vendedores(resultado):
+def nombre_vendedores(resultado):           # Guarda el nombre de los vendedores en una lista
     vendedores_d = []
     for vendedores in resultado:
         vendedores_d.append(resultado[vendedores]["employee_name"])
     return vendedores_d
 
-
-def nombre_clientes(resultado):
+def nombre_clientes(resultado):           # Guarda el nombre de los vendedores en una lista
     clientes_d = []
     for clientes in resultado:
         clientes_d.append(resultado[clientes]["customer_name"])
     return clientes_d
 
-
-def nombre_productos(resultado):
+def nombre_productos(resultado):         # Guarda el nombre de los productos en una lista
     productos_d = []
     for productos in resultado:
         productos_d.append(resultado[productos]["product_name"])
     return productos_d
 
-
-def porcentaje_ventas(resultado, lista, total):
+def porcentaje_ventas(resultado, lista, total):           # Calcula el porcentaje de las ventas
     vendedores = {}
     vendedores_por = {}
     suma = 0
@@ -66,14 +64,13 @@ def porcentaje_ventas(resultado, lista, total):
         finally:
             vendedores[persona] = suma
             suma = 0
-    print(vendedores)
     for persona in lista:
         porcentaje = (vendedores[persona]*100)/total
         vendedores_por[persona] = str(round(porcentaje, 2))+'%'
-    print(vendedores_por)
+    pprint.pprint(vendedores_por)
 
 
-def porcentaje_clientes(resultado, lista, total):
+def porcentaje_clientes(resultado, lista, total):                # Calcula el porcentaje de la ventas de los clientes
     clientes = {}
     clientes_por = {}
     suma = 0
@@ -86,16 +83,17 @@ def porcentaje_clientes(resultado, lista, total):
         finally:
             clientes[persona] = suma
             suma = 0
-    print(clientes)
+
     for persona in lista:
         porcentaje = (clientes[persona]*100)/total
         clientes_por[persona] = str(round(porcentaje, 2))+'%'
-    print(clientes_por)
+    pprint.pprint(clientes_por)
 
-
-def product(resultado, lista):
+def product(resultado, lista):                       # Calcula las cantidades de productos que hay, 
+                                                    #  ¡¡¡¡Falta hacer que saque los 5 que mas se han vendido!!!!
     nombre_productos = {}
     productos_cant = {}
+    mas_5 = {}
     suma = 0
     for producto in lista:
         try:
@@ -106,88 +104,59 @@ def product(resultado, lista):
         finally:
             nombre_productos[producto] = suma
             suma = 0
-    print(nombre_productos)
+    productos_mas_vendidos = nlargest(5, nombre_productos, key=nombre_productos.get)
+    pprint.pprint(f'Los 5 productos mas vendidos son: {productos_mas_vendidos}')
+    
+    
 
-
-def facturacion(resultado):
-    dict_meses = {"01": 0, "02": 0, "03": 0, "04": 0, "05": 0,
-                  "06": 0, "07": 0, "08": 0, "09": 0, "10": 0, "11": 0, "12": 0}
+def facturacion(resultado):             # Saca la facturación por mes
+    dict_meses = {"01": 0,"02": 0,"03": 0,"04": 0,"05": 0,"06": 0,"07": 0,"08": 0,"09": 0,"10": 0,"11": 0,"12": 0}
     for n in resultado:
         for mes in dict_meses:
             if resultado[n]["order_date"][5:7] in mes:
-                dict_meses[mes] += int(resultado[n]["quantity"].replace(',', '')) * \
-                    int(resultado[n]["unit_price"].replace(',', ''))
-    print(dict_meses)
+                dict_meses[mes] += int(resultado[n]["quantity"].replace(',','')) * int(resultado[n]["unit_price"].replace(',',''))
+    pprint.pprint(dict_meses)
 
 
-def menu():
+def pinta_menu():           # Pinta el menu
     print('------MENU------- ')
     print('1. % de ventas de cada vendedor')
     print('2. % sobre las ventas de cada cliente')
     print('3. 5 prod mas vendidos')
     print('4. Ingresos mensuales')
+    print('5. Salir')
 
+
+def limpia_pantalla():
+    os.system('clear')
 
 def main():
     limpia_pantalla()
     pinta_menu()
-    todos = leer_archivo()
-    while True:
+    datos = leer_archivo(archivo)
+    while True: 
         opcion = int(input('Seleccione una opción: '))
         if opcion == 1:
             limpia_pantalla()
-            listar_tareas(todos)
+            porcentaje_ventas(datos, nombre_vendedores(datos), total(datos))
             pinta_menu()
         if opcion == 2:
-            nueva_tarea(todos)
-            escribir_tareas(todos)
-            todos = leer_archivo()
             limpia_pantalla()
+            porcentaje_clientes(datos, nombre_clientes(datos), total(datos))
             pinta_menu()
         if opcion == 3:
-            numero = int(input('Introduzca el número de la tarea a editar: '))
-            editar_tarea(todos, numero)
-            escribir_tareas(todos)
-            todos = leer_archivo()
             limpia_pantalla()
+            product(datos,  nombre_productos(datos))
             pinta_menu()
         if opcion == 4:
-            numero = input('Introduzca el número de la tarea a borrar: ')
-            borrar_tarea(todos, numero)
-            escribir_tareas(todos)
-            todos = leer_archivo()
             limpia_pantalla()
+            facturacion(datos)
             pinta_menu()
         if opcion == 5:
-            numero = int(
-                input('Introduzca el estado [0-> Pendiente, 1-> Hecho]: '))
-            estado = ESTADOS[numero]
-            limpia_pantalla()
-            listar_tareas(todos, estado)
-            pinta_menu()
-        if opcion == 6:
             exit()
 
 
-leer_archivo(archivo)
-print()
-# nombre_vendedores(leer_archivo(archivo))
-print()
-# total(leer_archivo(archivo))
-print()
+
+main()
 
 
-porcentaje_ventas(leer_archivo(archivo), nombre_vendedores(
-    leer_archivo(archivo)), total(leer_archivo(archivo)))
-print()
-
-porcentaje_clientes(leer_archivo(archivo), nombre_clientes(
-    leer_archivo(archivo)), total(leer_archivo(archivo)))
-
-print()
-product(leer_archivo(archivo), nombre_productos(leer_archivo(archivo)))
-print()
-
-facturacion(leer_archivo(archivo))
-
-print()
