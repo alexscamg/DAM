e73fdfd7371445164fa8eb70fd97db6e78d556da
alexscamg <alejandrosanchezsk8@gmail.com>
2022-12-18 18:20:03 +0100

    Commit!

---
+++ examen/cuentavocales.py
@@ -0,0 +1,18 @@
+
+
+texto = input('Introduce un texto: ')
+
+def cuenta_vocales(texto):
+  vocales_texto = []
+  vocales = ['a','e','i','o','u']
+  for i in texto:
+    i = i.lower()
+    if i in vocales and i not in vocales_texto:
+      vocales_texto.append(i)
+    
+    
+  return vocales_texto
+
+
+
+print(cuenta_vocales(texto))
